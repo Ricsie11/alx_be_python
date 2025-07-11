7d18f95a7c0957a3ff50c0dff76f62ccfc80018b
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-07-11 19:19:27 +0100

    age declaration

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+#prompt the user for their current age
+current_age = int(input("Enter your current age; "))
+#future age calculation
+current_year = 2023
+future_year = 2050
+future_age = future_year - current_year + current_age
+#display the future age
+print("in 2050, you will be", future_age, "years old")
+
