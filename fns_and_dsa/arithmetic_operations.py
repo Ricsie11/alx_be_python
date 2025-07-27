ad599be2536a5b8d674f67e553f600b3839f23e5
unknown <wisdomalfred964@gmail.com>
2025-07-27 17:02:24 +0100

    Additional changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1: float, num2: float, operation: str) -> float:
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -13,3 +13,5 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 / num2
     else:
         return "Error: Invalid operation"
+
+
