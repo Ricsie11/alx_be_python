cf16528b4cd27a2457e42824dfe7189a78cf208a
unknown <wisdomalfred964@gmail.com>
2025-08-03 15:31:56 +0100

    simple test calculation

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,7 +15,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(3.5, 2.5), 6.0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test subtraction."""
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 0), 0)
