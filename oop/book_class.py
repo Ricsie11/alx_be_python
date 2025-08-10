716f02fe0d3f4ca471c7ce1b11d12dfdfdfebae6
unknown <wisdomalfred964@gmail.com>
2025-08-10 15:09:34 +0100

    Changes on repr

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,10 +7,10 @@ class Book:
     
     #Magic method
     def __str__(self):
-        return f'{self.title} by {self.author}, published {self.year}.'
+        return f'{self.title} by {self.author}, published in {self.year}'
     
     def __repr__(self):
-        return f"Book(' {self.title}', '{self.author}', {self.year})."
+        return f"Book('{self.title}', '{self.author}', {self.year})"
     
 
     #Destructor
